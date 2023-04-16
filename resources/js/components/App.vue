a3ce17eca00116038fbd3d52c9942da960cf431b
hainh <master.dkny1102@gmail.com>
2023-04-17 00:28:47 +0700

    Initial commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <div>test</div>
+        <router-link to="/">Go to Home</router-link>
+        <router-link to="/about">Go to About</router-link>
+        <router-view/>
+    </div>
+</template>
---
+++ resources/js/router/index.js
@@ -0,0 +1,24 @@
+import {
+    createWebHistory,
+    createRouter
+} from "vue-router";
+import Home from '../components/Home.vue'
+import About from '../components/About.vue'
+
+
+const history = createWebHistory();
+const routes = [
+    {
+        path: "/",
+        component: Home
+    },
+    {
+        path: "/about",
+        component: About
+    },
+];
+const router = createRouter({
+    history,
+    routes
+});
+export default router;
